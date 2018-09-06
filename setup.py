8d83db89f1a4b63cc09da8b8d1997f95d939d770
Andrew Cordery <cordery@gmail.com>
2018-09-06 15:00:04 -0600

    pypi tweaks

--- setup.py
+++ setup.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-from setuptools import setup
+import setuptools
 
 package_dir = {'': 'src'}
 
@@ -9,28 +9,27 @@ package_data = {'': ['*']}
 
 install_requires = ['django>=2.1,<3.0']
 
-long_description = open("README.rst", "r").read()
+with open("README.rst", "r") as fh:
+    long_description = fh.read()
 
-setup_kwargs = {
-    'name': 'django-conditional-views',
-    'version': '0.1.1',
-    'description': 'Simple Etag and Last-Modified mixins for class based views.',
-    'long_description': long_description,
-    'author': 'Andrew Cordery',
-    'author_email': 'cordery@gmail.com',
-    'url': 'https://github.com/cordery/django-conditional-views',
-    'package_dir': package_dir,
-    'packages': packages,
-    'package_data': package_data,
-    'install_requires': install_requires,
-    'python_requires': '>=3.6,<4.0',
-    'classifiers': [
+setuptools.setup(
+    name='django-conditional-views',
+    version='0.1.1',
+    description='Simple Etag and Last-Modified mixins for class based views.',
+    long_description=long_description,
+    author='Andrew Cordery',
+    author_email='cordery@gmail.com',
+    url='https://github.com/cordery/django-conditional-views',
+    package_dir=package_dir,
+    packages=packages,
+    package_data=package_data,
+    install_requires=install_requires,
+    python_requires='>=3.6,<4.0',
+    classifiers=[
         "Framework :: Django",
         "Framework :: Django :: 2.1",
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
-    ],
-}
-
-setup(**setup_kwargs)
+    ]
+)
